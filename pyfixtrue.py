f236e394d8b74e460ae9648757272dbe6014f64c
zhouliudong <280906165@qq.com>
2024-10-09 10:16:50 +0800

    pytest提交

--- pyfixtrue.py
+++ pyfixtrue.py
@@ -2,6 +2,7 @@ import pytest
 
 @pytest.mark.parametrize('x', [1, 2])
 @pytest.mark.parametrize('y', [4, 5])
-# @pytest.mark.parametrize('z', [7, 8])
+@pytest.mark.parametrize('z', [7, 8])
 def test(x, y, c):
     print('x=%d, y=%d, z=%d' % (x, y, z))
+    print(123)
